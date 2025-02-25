c0da45f8f7c983eaaebb8118e0919e73e2adb2e5
Javohir <hp15032004@gmail.com>
2025-02-25 20:52:11 +0500

    Initial commit

---
+++ majburiyqowiw.py
@@ -0,0 +1,23 @@
+import asyncio
+from aiogram import Bot, Dispatcher, F
+from aiogram.types import Message
+from aiogram.filters import Command
+
+TOKEN = "7763814340:AAHZd5_wuXq8hv00nAZAVa548t6YrcZWkWY"
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def send_welcome(message: Message):
+    await message.answer("Assalomu alaykum!")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+
+
+
